7c80be50418d8e8c1a126c77a2a5a17bb5a3ba52
Tag <tagadvance+github.com@gmail.com>
2025-11-01 20:47:59 -0600

    Source utility

---
+++ source.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+##
+## Checks the "configuration tree" for the specified paths and prints the first readable path.
+##
+
+usage() {
+  echo "Usage: $0 <path1> [additional paths ...]"
+  echo "  path1: The path to source, e.g. '.config'"
+  echo "  path2: An optional path to source, e.g. 'foo/.config'"
+}
+
+if [ "$#" -lt 1 ]; then
+  echo "Error: Missing arguments." >&2
+  usage
+  exit 1
+fi
+
+paths=(
+  "."
+  # the directory containing the script
+  "$(dirname "$(readlink -f "$0")")"
+  "$HOME"
+  "/etc"
+  "/root"
+)
+
+for arg in "$@"; do
+  for path in "${paths[@]}"; do
+    if [[ -r "$path/$arg" ]]; then
+      echo "$path/$arg";
+      break 2
+    fi
+  done
+done
